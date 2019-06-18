42559041aa3aec2f54078c9ca57c825c71ab3fa4
Tad <tad@spotco.us>
2019-06-18 13:26:50 -0400

    Going the distance... [pt2]

--- app/src/main/java/us/spotco/maps/MainActivity.java
+++ app/src/main/java/us/spotco/maps/MainActivity.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2017 Divested Computing, Inc.
+Copyright (c) 2017 Divested Computing Group
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
