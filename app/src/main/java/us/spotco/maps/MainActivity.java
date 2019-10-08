33af6e4e2a93a4edc82a9c447d679234d82ce431
Tad <tad@spotco.us>
2019-10-08 01:05:42 -0400

    2019

--- app/src/main/java/us/spotco/maps/MainActivity.java
+++ app/src/main/java/us/spotco/maps/MainActivity.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2017 Divested Computing Group
+Copyright (c) 2017-2019 Divested Computing Group
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
